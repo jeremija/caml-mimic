13ff99b2315674d9e4abfeeee31a7bd1e0069bb1
Jerko Steiner <jerko.steiner@nyumc.org>
2018-02-26 13:25:54 -0500

    Use relative paths in constants.py

--- constants.py
+++ constants.py
@@ -8,8 +8,8 @@ EMBEDDING_SIZE = 100
 MAX_LENGTH = 2500
 
 #where you want to save any models you may train
-MODEL_DIR = path.join(path.join(__file__, 'saved_models'))
+MODEL_DIR = path.join(path.dirname(__file__), 'saved_models')
 
-DATA_DIR = path.join(path.join(__file__, 'mimicdata/'))
-MIMIC_3_DIR = path.join(path.join(__file__, 'mimicdata/mimic3'))
-MIMIC_2_DIR = path.join(path.join(__file__, 'mimicdata/mimic2'))
+DATA_DIR = path.join(path.dirname(__file__), 'mimicdata/')
+MIMIC_3_DIR = path.join(path.dirname(__file__), 'mimicdata/mimic3')
+MIMIC_2_DIR = path.join(path.diranem(__file__), 'mimicdata/mimic2')
